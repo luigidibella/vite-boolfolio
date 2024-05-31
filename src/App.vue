35e1d7ea36d80c72da809fb48a4b7e45ad1226ff
luigidibella <lykos.omega@gmail.com>
2024-05-31 18:29:27 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 import axios from 'axios';
 
 export default {
+  
+  data(){
+    return {
+      'projects' : []
+    }
+  },
 
   methods:{
     getApi(){
       console.log('get API');
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then(result => {
+          this.projects = result.data
+          console.log(this.projects);
+        })
+        .catch(error =>{
+          console.log(error);
+        })
     }
   },
 
@@ -18,7 +32,18 @@ export default {
 
 <template>
   <div class="main-wrapper">
-    <h1>boolfolio home</h1>
+    <div class="container">
+      <h1>boolfolio home</h1>
+      <h3>i miei progetti:</h3>
+      <ul>
+        <li 
+          v-for="project in projects" 
+          :key="project.id"
+        >
+        {{project.id}} - {{project.title}}</li>
+        
+      </ul>
+    </div>
   </div>
 </template>
 
