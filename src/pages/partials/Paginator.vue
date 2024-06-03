d71a0bbf41cdecfef521c3514966c396b2df5b71
luigidibella <lykos.omega@gmail.com>
2024-06-03 16:04:22 +0200

    [add] Paginator

---
+++ src/pages/partials/Paginator.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+  
+  name: 'Paginator',
+  props: {
+    data:Object
+  },
+  methods: {
+    callApi(link){
+      // console.log(link);
+      this.$emit('callApi', link)
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="paginator">
+    <button
+      v-for="link in data.links"
+      :key="link.label"
+      v-html="link.label"
+      :disabled="link.active || (!link.url && link.label !== '...')"
+      @click="callApi(link.url)"
+    >
+    </button>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.paginator{
+  margin-top: 20px;
+  button{
+    padding: 5px 10px;
+    margin-right: 5px;
+  }
+}
+  
+</style>
