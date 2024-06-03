afa11aa02266285127ba61d41821c31508c2888b
luigidibella <lykos.omega@gmail.com>
2024-06-03 15:59:22 +0200

    [add] Header

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><router-link :to="{name: 'home'}">Home</router-link></li>
+          <li><router-link :to="{name: 'about'}">About</router-link></li>
+          <li><router-link :to="{name: 'projects'}">Projects</router-link></li>
+          <li><router-link :to="{name: 'contacts'}">Contacts</router-link></li>
+        </ul>
+      </nav>
+    </header> 
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+header{
+  ul{
+    display: flex;
+    li{
+      list-style: none;
+      a{
+        color: white;
+        text-decoration: none;
+        padding: 0 20px;
+        text-transform: uppercase;
+      }
+    }
+  }
+}
+  
+</style>
