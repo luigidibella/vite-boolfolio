2c5f4348bfcef664980311cd999f8f30de7549dd
luigidibella <lykos.omega@gmail.com>
2024-06-05 01:17:54 +0200

    [add] Type and Technology Projects page

---
+++ src/pages/TechnologyProjects.vue
@@ -0,0 +1,81 @@
+<script>
+import { store } from '../data/store';
+import axios from 'axios';
+import Loader from '../components/partials/Loader.vue';
+
+
+export default {
+  
+  name: 'TechnologyProjects',
+  components:{
+    Loader,
+  },
+  data(){
+    return{
+      technologyName: '',
+      Projects: [],
+      loading: true,
+    }
+  },
+  methods: {
+    getApi(slug){
+      axios.get(store.apiUrl + 'projects-by-technology' + '/' + slug)
+      .then(result => {
+        this.loading = false;
+        this.technologyName = result.data.name;
+        this.projects = result.data.projects;
+      })
+      .catch(error => {
+        this.loading = false;
+        console.log(error);
+      })
+    }
+  },
+  mounted() {
+    this.getApi(this.$route.params.slug)
+  },
+}
+
+</script>
+
+<template>
+  <div class="main-wrapper">
+    <h1>Elenco Progetti: {{ technologyName }}</h1>
+
+    <div class="d-flex-center" v-if="loading">
+      <Loader />
+    </div>
+
+    <div class="container" v-else>
+      <ul>
+        <li 
+          v-for="project in projects" 
+          :key="project.id"
+        >
+          {{ project.id }} - 
+          <router-link :to="{ name: 'projectDetail', params:{slug: project.slug} }">{{ project.title }}</router-link>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.main-wrapper{
+  .container{
+    display: flex;
+    justify-content: start;
+    min-height: 532px;
+    ul{
+      list-style: none;
+      padding: 0 5px;
+      li{
+        margin: 5px 0;
+      }
+    }
+  }
+}
+
+</style>
